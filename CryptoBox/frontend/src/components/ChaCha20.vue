8f46f0b7a73e7c7c289047722950922b26000b5d
CodeMax <CodeMax@web3mq.com>
2023-01-28 20:15:08 +0800

    Add x25519 and chacha20 feature

--- CryptoBox/frontend/src/components/ChaCha20.vue
+++ CryptoBox/frontend/src/components/ChaCha20.vue
@@ -1,26 +1,95 @@
 
 <template>
-    <main>
-       <div>
-          ChaCha20
-       </div>
-    </main>
- </template>
-   
- <script lang="ts" setup>
- import { reactive } from 'vue'
- import { Greet } from '../../wailsjs/go/main/App'
+   <main>
+      <div style="display: block; margin: 0 auto">
+         <el-col>
+            <el-card shadow="hover">
+               <el-button type="primary" @click="EncryptResultAction">Encrypt</el-button>
+               <div style="margin-top: 20px">
+               </div>
+             
+               <div style="display: flex">
+                  <el-button size="large" @click="RandomKeyAction">Random Key</el-button>&nbsp;&nbsp;&nbsp;&nbsp;
+                  <el-input size="large" maxlength="128" show-word-limit v-model="ChaCha20Key" placeholder="Key" />
+               </div>
+
+               <div style="margin-top: 20px"></div>
+               <div style="display: flex">
+                  <el-button size="large" @click="RandomNonceAction">Random Nonce</el-button>&nbsp;&nbsp;
+                  <el-input size="large" maxlength="128" show-word-limit v-model="ChaCha20Nonce" placeholder="Nonce" />
+               </div>
+
+               <div style="margin-top: 20px"></div>
+               <el-input size="large" maxlength="128" show-word-limit v-model="Payload" rows="6" type="textarea"
+                  placeholder="Payload" spellcheck="false" />
+
+               <div style="margin-top: 20px">
+               </div>
+               <el-input size="large" maxlength="128" show-word-limit v-model="EncryptResult" rows="6" type="textarea"
+                  placeholder="Result" />
+            </el-card>
+         </el-col>
+      </div>
+   </main>
+</template>
  
- const data = reactive({
-    name: "",
-    resultText: "Please enter your name below 👇",
- })
- 
- function greet() {
-    Greet(data.name).then(result => {
-       data.resultText = result
-    })
- }
- 
- </script>
- 
+<script lang="ts" setup>
+import { ref } from 'vue'
+import { ElMessage } from 'element-plus';
+
+// @ts-ignore
+// import * as stdlib from '@stablelib/chacha';
+import JSChaCha20 from "js-chacha20";
+
+import { CreateX25519KeyPair } from '../service';
+
+function Buf2Hex(buffer: ArrayBuffer) { // buffer is an ArrayBuffer
+   return [...new Uint8Array(buffer)]
+      .map(x => x.toString(16).padStart(2, '0'))
+      .join('');
+}
+
+const Payload = ref("");
+const EncryptResult = ref("");
+
+const ChaCha20Key = ref("");
+
+const ChaCha20Nonce = ref("");
+
+
+const RandomNonceAction = async () => {
+   const array = new Uint8Array(12);
+   let resultBytes = crypto.getRandomValues(array);
+
+
+   ChaCha20Nonce.value = Buf2Hex(resultBytes);
+}
+
+const fromHexString = (hexString: any) =>
+  Uint8Array.from(hexString.match(/.{1,2}/g).map((byte: string) => parseInt(byte, 16)));
+
+
+const RandomKeyAction = async () => {  
+   const array = new Uint8Array(32);
+   let resultBytes = crypto.getRandomValues(array);
+
+   ChaCha20Key.value = Buf2Hex(resultBytes);
+}
+
+const Uint8ToBase64String = (u8a: any) => {
+  return btoa(String.fromCharCode.apply(null, u8a));
+};
+
+const EncryptResultAction = async () => {
+   console.log( "====");
+
+   // let result = stdlib.stream(fromHexString(ChaCha20Key.value),  fromHexString(ChaCha20Nonce.value), new Uint8Array() );
+
+   let dataByte = new TextEncoder().encode(Payload.value);
+
+   const result = new JSChaCha20(fromHexString(ChaCha20Key.value), fromHexString(ChaCha20Nonce.value)).encrypt(dataByte);
+
+   console.log(result, "====");
+   EncryptResult.value = Uint8ToBase64String(result);
+}
+</script>
--- CryptoBox/frontend/src/components/X25519.vue
+++ CryptoBox/frontend/src/components/X25519.vue
@@ -7,13 +7,34 @@
                <el-button type="primary" @click="GenerateKeyResult">Generate</el-button>
                <div style="margin-top: 20px">
                </div>
-               <el-input size="large" maxlength="128" show-word-limit v-model="PrivateKey" rows="6" type="textarea"
-                  placeholder="PrivateKey" />
+               <span class="ml-3 w-35 text-gray-600 inline-flex items-center">Alice PrivateKey</span>
+               <el-input size="large" maxlength="128" show-word-limit v-model="AlicePrivateKey" placeholder="PrivateKey" />
 
                <div style="margin-top: 20px">
                </div>
-               <el-input size="large" maxlength="128" show-word-limit v-model="PublicKey" rows="6" type="textarea"
-                  placeholder="PublicKey" />
+               <span class="ml-3 w-35 text-gray-600 inline-flex items-center">Alice PublicKey</span>
+               <el-input size="large" maxlength="128" show-word-limit v-model="AlicePublicKey" placeholder="PublicKey" />
+
+               <div style="margin-top: 20px">
+               </div>
+
+               <span class="ml-3 w-35 text-gray-600 inline-flex items-center">Bob PrivateKey</span>
+               <el-input size="large" maxlength="128" show-word-limit v-model="BobPrivateKey" placeholder="PrivateKey" />
+
+               <div style="margin-top: 20px">
+               </div>
+               <span class="ml-3 w-35 text-gray-600 inline-flex items-center">Bob PublicKey</span>
+               <el-input size="large" maxlength="128" show-word-limit v-model="BobPublicKey" placeholder="PublicKey" />
+            </el-card>
+            <div style="margin-top: 20px">
+               </div>
+
+
+            <el-card shadow="hover">
+               <el-button type="primary" @click="GenerateShareKeyResult">Get ShareKey</el-button>
+               <div style="margin-top: 20px">
+               </div>
+               <el-input size="large" maxlength="128" show-word-limit v-model="ShareKey" placeholder="ShareKey" />
             </el-card>
          </el-col>
       </div>
@@ -26,17 +47,56 @@ import { ElMessage } from 'element-plus';
 
 import { CreateX25519KeyPair } from '../service';
 
+// Common.js and ECMAScript Modules (ESM)
+import * as ed from '@noble/ed25519';
 
-const PrivateKey = ref("");
-const PublicKey = ref("");
+const Uint8ToBase64String = (u8a: any) => {
+  return btoa(String.fromCharCode.apply(null, u8a));
+};
 
+const ByteArrayToHexString = (byteArray: Uint8Array) => {
+  return Array.from(byteArray, function (byte) {
+    return ('0' + (byte & 0xff).toString(16)).slice(-2);
+  }).join('');
+};
+
+
+const GetShareKeyResult = async (privateKeyA: any, pubKeyB: any, format: string) => {
+  try {
+    const shared = await ed.curve25519.scalarMult(privateKeyA, pubKeyB);
+    if (format === 'hex') {
+      return ByteArrayToHexString(shared);
+    } else {
+      return Uint8ToBase64String(shared);
+    }
+  } catch (e: any) {
+    return e.toString();
+  }
+};
+
+const ShareKey = ref("");
+
+const AlicePrivateKey = ref("");
+const AlicePublicKey = ref("");
+
+
+const BobPrivateKey = ref("");
+const BobPublicKey = ref("");
 
 const GenerateKeyResult = async () => {
    let resp = await CreateX25519KeyPair();
 
-   PrivateKey.value = resp.PrivateKey;
-   PublicKey.value = resp.PublicKey;
+   AlicePrivateKey.value = resp.PrivateKey;
+   AlicePublicKey.value = resp.PublicKey;
 
+   resp = await CreateX25519KeyPair();
+
+   BobPrivateKey.value = resp.PrivateKey;
+   BobPublicKey.value = resp.PublicKey;
 }
 
+const GenerateShareKeyResult = async() => {
+   let result = await GetShareKeyResult(AlicePrivateKey.value, BobPublicKey.value, "hex");
+   ShareKey.value = result;
+}
 </script>
